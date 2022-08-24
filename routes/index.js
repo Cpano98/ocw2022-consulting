712c4e8d5d3d5d022f63d528a65a892336f1d6c5
Carlos Pano <carlos.pano@oracle.com>
2022-08-24 14:36:45 -0500

    Nav creation

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Home" });
+});
+
+router.get("/solutions", function (req, res, next) {
+  res.render("page1", { title: "Solutions" });
+});
+
+router.get("/news", function (req, res, next) {
+  res.render("page2", { title: "News" });
+});
+
+router.get("/community", function (req, res, next) {
+  res.render("page3", { title: "Community" });
+});
+
+router.get("/contact", function (req, res, next) {
+  res.render("page4", { title: "Contact" });
 });
 
 module.exports = router;
