e65e4df0596940d0e2074efc4fce2d60bd95e8f7
Carlos Pano <carlos.pano@oracle.com>
2022-08-29 16:33:18 -0500

    Blur transition

--- public/javascripts/animation.js
+++ public/javascripts/animation.js
@@ -12,20 +12,18 @@ function delay(n) {
 function pageTransition() {
   var tl = gsap.timeline();
   tl.to(".loading-screen", {
-    duration: 1.2,
-    width: "100%",
-    left: "0%",
+    duration: 0.5,
+    opacity: 1,
     ease: "Expo.easeIn",
   });
 
   tl.to(".loading-screen", {
     duration: 1,
-    width: "100%",
-    left: "100%",
+    opacity: 0,
     ease: "Expo.easeInOut",
     delay: 0.3,
   });
-  tl.set(".loading-screen", { left: "-100%" });
+  tl.set(".loading-screen", { opacity: "0" });
 }
 
 function contentAnimation() {
