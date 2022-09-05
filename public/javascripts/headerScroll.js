42bd6b7f640d673aeacfdef091d25f57a4a5f8ad
Carlos Pano <carlos.pano@oracle.com>
2022-09-05 16:28:22 -0500

    New SEO improvements

--- public/javascripts/headerScroll.js
+++ public/javascripts/headerScroll.js
@@ -1,15 +1,16 @@
-window.onscroll = function () {
-  changeHeaderColor();
-};
+window.addEventListener("load", (event) => {
+  window.onscroll = function () {
+    changeHeaderColor();
+  };
 
-function changeHeaderColor() {
+  function changeHeaderColor() {
     let header = document.getElementById("mainNavigation");
-  if (document.documentElement.scrollTop > 150) {
-    header.classList.add("headerBackground");
-    header.classList.remove("headerBackgroundTrans");
-  } else {
-    header.classList.remove("headerBackground");
-    header.classList.add("headerBackgroundTrans");
-    
+    if (document.documentElement.scrollTop > 150) {
+      header.classList.add("headerBackground");
+      header.classList.remove("headerBackgroundTrans");
+    } else {
+      header.classList.remove("headerBackground");
+      header.classList.add("headerBackgroundTrans");
+    }
   }
-}
+});
