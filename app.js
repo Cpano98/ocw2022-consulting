92096cb6793c7456df71f329fe2356e8828f0138
Carlos Pano <carlos.pano@oracle.com>
2022-09-05 15:41:08 -0500

    New Server

--- app.js
+++ app.js
@@ -38,9 +38,10 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-app.listen(process.env.PORT || 3000, function () {
+app.listen(process.env.PORT || 3001, function () {
   console.log("Success! Server running on port 3001");
-  console.log("Enter application on: http://localhost:3000/");
+  console.log("Enter application on: http://localhost:3001/");
+  console.log("OCI public: http://129.153.93.141:3001/");
 });
 
 module.exports = app;
