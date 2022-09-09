abbc010462acffeab4eb665d1fabb029b5247240
Carlos Pano <carlos.pano@oracle.com>
2022-09-09 12:10:22 -0500

    New Jquery

--- app.js
+++ app.js
@@ -57,7 +57,7 @@ const sslServer = https.createServer({
 sslServer.listen(3001, function(){
   console.log("Success! Server running on port 3001");
   console.log("Enter application on: https://localhost:3001/");
-  console.log("OCI public: http://129.153.93.141:3000/");
+  console.log("OCI public: https://129.153.93.141/");
 });
 
 module.exports = app;
